9e7e530693ef2e6559670b8a7fe93d4484404e8b
ananduremanan <ananduor@gmail.com>
2023-09-14 22:08:00 +0530

    Added all CRUD operations

--- src/app/components/RemoveBtn.tsx
+++ src/app/components/RemoveBtn.tsx
@@ -1,6 +1,25 @@
+"use client";
 import React from "react";
 import { MdDelete } from "react-icons/md";
+import { useRouter } from "next/navigation";
 
-export default function RemoveBtn() {
-  return <button className="text-red-500">{<MdDelete size={20} />}</button>;
+export default function RemoveBtn({ id, isDeleted }: any) {
+  const router = useRouter();
+  const removeNote = async () => {
+    const confirmed = confirm("Are You sure?");
+    if (confirmed) {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/notes?id=${id}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        isDeleted(true);
+      }
+    }
+  };
+
+  return (
+    <button className="text-red-500" onClick={removeNote}>
+      {<MdDelete size={20} />}
+    </button>
+  );
 }
--- src/app/editTopic/[id]/page.tsx
+++ src/app/editTopic/[id]/page.tsx
@@ -1,5 +1,42 @@
+"use client"
+import React, { useState, useEffect } from "react";
 import EditNoteForm from "@/app/components/EditNoteForm";
 
-export default function EditTopic() {
-  return <EditNoteForm />;
+const getNoteById = async (id: any) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/notes/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Error fetching note");
+    }
+
+    console.log(res.json);
+    return res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default function EditTopic({ params }: any) {
+  const { id } = params;
+  const [note, setNote] = useState(null);
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      const data = await getNoteById(id);
+      setNote(data.note);
+    };
+
+    fetchNote();
+  }, [id]);
+
+  if (!note) {
+    return <div>Loading...</div>;
+  }
+
+  const { title, description } = note;
+
+  return <EditNoteForm id={id} title={title} description={description} />;
 }
